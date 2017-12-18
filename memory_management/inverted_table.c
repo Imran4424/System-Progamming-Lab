aa6507d5024591b16e92e0e84a13330a55acd605
Imran4424 <akashganga.programming.534363@gmail.com>
2017-12-19 02:07:58 +0600

    inverted table complete

--- memory_management/inverted_table.c
+++ memory_management/inverted_table.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+const main_memory = 32;
+const frame_size = 4;
+const page_table_size = main_memory/frame_size;
+
+int checker[] = {0};
+
+int ram[] = {3, 5, 6, 2, 6, 1, 11, 10, 6, 100, 1, -11, 77, 34, 19, 9, 22, 10, 6, 33, 12, 06, 28, 68,29, 28, 59, 22, 85, 36};
 
 typedef struct page_table{
 	int pID;
@@ -7,9 +14,44 @@ typedef struct page_table{
 } ptable;
 
 
+ptable table[page_table_size];
+
+void g_page_table()
+{
+	int j = 10000;
+
+	for(int i = 0;i<page_table_size;i++)
+	{
+		table[i].pID = rand() % 3 + j;
+		table[i].page_number = i % 2;
+
+		j += 3;
+	}
+}
+
+
 int main()
 {
+	g_page_table();
 
+	printf("insert an pID and logical address\n");
+	int pID, logical_address;
+
+	scanf("%d %d",&pID,&logical_address);
+
+	int p = logical_address / frame_size;
+	int d = logical_address % frame_size;
+
+	int i = 0;
+ 
+	while(table[i].pID != pID || table[i].page_number != p)
+	{
+		i++;
+	}
+
+	printf("frame no: %d  offset no: %d\n",i,d);
+
+	printf("Data is: %d \n",ram[i*frame_size+ d - 1]);
 
 	return 0;
 }
