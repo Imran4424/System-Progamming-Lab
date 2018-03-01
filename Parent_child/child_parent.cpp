bac1a87a151150b56d10f5b8e1a8b3bbd1f732d2
Imran4424 <akashganga.programming.534363@gmail.com>
2018-03-01 08:37:23 +0600

    5 child from a single parent

--- Parent_child/child_parent.cpp
+++ Parent_child/child_parent.cpp
@@ -1,5 +1,27 @@
+/*
+	Here we implement a code to create child proecess
+
+	here we create 5 child process from a single parent
+
+	here "fork()" function is called to create the child process
+
+	after calling fork(),
+
+						it will return the child proceess id to the parent process
+
+						it will return 0 to the child process
+
+	we use "getpid()" function to achieve current process id, for both parent and child process
+
+
+	we use one more function for child process
+	that is "getppid()"
+	to achieve parent process id for this child
+
+*/
+
 #include<stdio.h>
-#include<unistd.h>
+#include<unistd.h>      // For getpid(), getppid(), fork()
 #include<sys/wait.h>
 #include <iostream>
 using namespace std;
@@ -20,7 +42,7 @@ int main()
 	}
 	else if(childPID == 0 )
 	{
-		printf("#######I am child One - %d,My parent is  %d \n\n", childPID, myPID);
+		printf("#######I am child One - %d,My parent is  %d \n\n",myPID,getppid());
 	}
 	else
 	{
@@ -35,7 +57,7 @@ int main()
 		}
 		else if(childPID2 == 0 )
 		{
-			printf("#######I am child two - %d,My parent is  %d \n\n", childPID2, myPID);
+			printf("#######I am child two - %d,My parent is  %d \n\n", myPID,getppid());
 		}
 		else
 		{
@@ -51,7 +73,7 @@ int main()
 			}
 			else if(childPID3 == 0 )
 			{
-				printf("#######I am child three- %d,My parent is  %d \n\n", childPID3, myPID);
+				printf("#######I am child three- %d,My parent is  %d \n\n",myPID,getppid());
 			}
 			else
 			{
@@ -68,7 +90,7 @@ int main()
 				}
 				else if(childPID4 == 0 )
 				{
-					printf("#######I am child four- %d,My parent is  %d \n\n", childPID4, myPID);
+					printf("#######I am child four- %d,My parent is  %d \n\n", myPID, getppid());
 				}
 				else
 				{
@@ -84,49 +106,46 @@ int main()
 					}
 					else if(childPID5 == 0 )
 					{
-						printf("#######I am child five- %d,My parent is  %d \n\n", childPID5, myPID);
+						printf("#######I am child five- %d,My parent is  %d \n\n", myPID,getppid());
 					}
 					else
 					{
 
 
 				
-						printf("\nI am parent of child %d, My PID is %d .\n\n", childPID5, myPID);
+						printf("\nI am parent of child %d, My PID is %d .\n\n", childPID5, getppid());
 						wait(NULL);
 						printf("My child is really dead. %d\n\n",childPID5);
 					}
 
 
 				
-					printf("\nI am parent of child %d, My PID is %d .\n\n", childPID4, myPID);
+					printf("\nI am parent of child %d, My PID is %d .\n\n", childPID4, getppid());
 					wait(NULL);
 					printf("My child is really dead. %d\n\n",childPID4);
 				}
 
 				
-				printf("\nI am parent of child %d, My PID is %d .\n\n", childPID3, myPID);
+				printf("\nI am parent of child %d, My PID is %d .\n\n", childPID3, getppid());
 				wait(NULL);
 				printf("My child is really dead. %d\n\n",childPID3);
 			}
 
 
 
-			printf("\nI am parent of child %d, My PID is %d .\n\n", childPID2, myPID);
+			printf("\nI am parent of child %d, My PID is %d .\n\n", childPID2, getppid());
 			wait(NULL);
 			printf("My child is really dead. %d\n\n",childPID2);
 		}
 
 
-		printf("\nI am parent of child %d, My PID is %d .\n\n", childPID, myPID);
+		printf("\nI am parent of child %d, My PID is %d .\n\n", childPID, getppid());
 		wait(NULL);
 		printf("My child is really dead. %d\n\n",childPID);
 	}
 
 
-	while(1)
-	{
-		cout<<"loop"<<endl;
-	}	
+	while(1); //infinity loop to hold the program to view pstree -p
 
 	return 0;
 }
