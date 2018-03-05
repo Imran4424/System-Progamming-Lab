58160433cc8a1a0b78c8c75aa5ea25371811723a
Imran4424 <akashganga.programming.534363@gmail.com>
2018-03-05 10:38:55 +0600

    system programming

--- Parent_child/zoombie_process.c
+++ Parent_child/zoombie_process.c
@@ -15,15 +15,15 @@ int main(int argc, char const *argv[])
 	}
 	else if(childPID == 0)
 	{
-		//sleep(30);
 
 		printf("I am child Process.My pid is: %d\n", getpid());
 
 		exit(0);
+
+		sleep(100);
 	}
 	else
 	{
-		sleep(5);
 
 		printf("I am parent process.my pid: %d\n", getpid());
 		printf("My child process is: %d\n",childPID);
