8bfe0c8e69cd25b3b294a6769bb55e575642674f
Imran4424 <akashganga.programming.534363@gmail.com>
2018-03-05 09:54:09 +0600

    system programming

--- Parent_child/orphgan_child.c
+++ Parent_child/orphgan_child.c
@@ -4,7 +4,24 @@
 
 int main(int argc, char const *argv[])
 {
-	
+	pid_t childPID;
+
+	childPID = fork();
+
+	if(childPID < 0)
+	{
+		printf("child create failed\n");
+	}
+	else if(childPID == 0)
+	{
+		printf("I am child process.My pid is %d\n",getpid());
+
+		while(1);
+	}
+	else
+	{
+		printf("I am parent process.My pid is %d\n",getpid());
+	}
 
 
 	return 0;
