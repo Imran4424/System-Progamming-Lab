5b562354e5230150ee9f512097e3557a6d0648a7
Imran4424 <akashganga.programming.534363@gmail.com>
2018-03-06 00:29:21 +0600

    system programming

--- argvTest.cpp
+++ argvTest.cpp
@@ -5,7 +5,9 @@ int main(int ar_numbers,char *argv[])
 {
 	printf("Number of parameters: %d\n",ar_numbers);
 	
-	printf("%s %s %s %s\n",argv[0],argv[1],argv[2],argv[3]);
+	//printf("%s %s %s %s\n",argv[0],argv[1],argv[2],argv[3]);
+
+	for(int i = 0;)
 
 
 	return 0;
