8e72dac56d835fd7e0e0927d3ea91773334d69fd
Imran4424 <akashganga.programming.534363@gmail.com>
2018-03-06 00:32:18 +0600

    system programming

--- argvTest.cpp
+++ argvTest.cpp
@@ -7,8 +7,12 @@ int main(int ar_numbers,char *argv[])
 	
 	//printf("%s %s %s %s\n",argv[0],argv[1],argv[2],argv[3]);
 
-	for(int i = 0;)
+	for(int i = 0;i<ar_numbers;i++)
+	{
+		printf("%s \n",argv[i] );
+	}
 
+	printf("\n");
 
 	return 0;
 }
