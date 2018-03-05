819a56e175e9a4f2b2917ac39961d035a2e76a03
Imran4424 <akashganga.programming.534363@gmail.com>
2018-03-05 18:35:56 +0600

    system programming

---
+++ Parent_child/ct_solve2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+	fork();
+
+	printf("My pid: %d\n", getpid());
+
+	fork();
+
+	printf("My parent's PID: %d\n", getppid());
+
+	return 0;
+}
